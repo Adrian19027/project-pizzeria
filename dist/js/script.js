c5b137e8f37c2e96cd9499e013095f3e823acdda
Adrian Kotowski <adekkotowski1@wp.pl>
2025-03-13 14:50:46 +0100

    Add display of menu products to page

--- dist/js/script.js
+++ dist/js/script.js
@@ -53,10 +53,28 @@ const select = {
   };
 
   class Product{
-    constructor() {
+    constructor(id, data){
       const thisProduct = this;
 
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+
       console.log('new Product:', thisProduct);
+    }
+
+    renderInMenu() {
+      const thisProduct = this;
+
+      /* generate HTML based on template */
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      /* create element using utils.createElementFromHTML */
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      /* find menu container */
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      /* add element to menu */
+      menuContainer.appendChild(thisProduct.element);
     }
   }
 
@@ -64,17 +82,20 @@ const select = {
     initMenu: function () {
       const thisApp = this;
 
-      console.log('thisApp.data:',thisApp.data);
-      const testProduct = new Product();
-      console.log('test Product:',testProduct);
-    },
+      console.log('thisApp.data:', thisApp.data);
+
+      for (let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+
+      },
 
     initData: function () {
       const thisApp = this;
 
       thisApp.data = dataSource;
     },
-
+    
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
