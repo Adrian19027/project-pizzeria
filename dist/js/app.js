4e39558aa6e1bf008806eb4e28e3edf666f53287
Adrian Kotowski <adekkotowski1@wp.pl>
2025-03-28 12:41:09 +0100

    Fix file paths

--- dist/js/app.js
+++ dist/js/app.js
@@ -23,7 +23,7 @@ const app = {
         thisApp.productList = document.querySelector(select.containerOf.menu);
 
         thisApp.productList.addEventListener('add-to-cart', function (event) {
-          app.cart.add(event.detail.product.thisProduct.prepareCartProduct());
+          app.cart.add(event.detail.product);
         });
     },
 
